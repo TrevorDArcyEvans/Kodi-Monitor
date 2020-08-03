38013d6c2b274a1406bb37f10b1b98790b0e0d2d
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-08-03 18:03:29 +0100

    - initial version

---
+++ kodiMonitor.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+from time import sleep
+import RPi.GPIO as GPIO
+
+# input buttons
+KodiUp = 14
+KodiBack = 18
+KodiLeft = 4
+
+# The only thing that is special about GPIO27 on Pin 13 is that it was GPIO21 on Rev 1 boards
+#   https://www.raspberrypi.org/forums/viewtopic.php?t=36486
+#
+# $ cat /sys/firmware/devicetree/base/model 
+# Raspberry Pi Model B Rev 1
+KodiEnter = 21
+
+KodiRight = 17
+KodiDown = 15
+
+# disable warnings
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(KodiUp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(KodiBack, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(KodiLeft, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(KodiEnter, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(KodiRight, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(KodiDown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+    if (GPIO.input(KodiUp) == True):
+        print('KodiUp')
+
+    if (GPIO.input(KodiBack) == True):
+        print('KodiBack')
+
+    if (GPIO.input(KodiLeft) == True):
+        print('KodiLeft')
+
+    if (GPIO.input(KodiEnter) == True):
+        print('KodiEnter')
+
+    if (GPIO.input(KodiRight) == True):
+        print('KodiRight')
+
+    if (GPIO.input(KodiDown) == True):
+        print('KodiDown')
+
+    sleep(0.15);
+
