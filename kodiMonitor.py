f7832d3e2288a0af16e36ddfbb49a42fc99b26ff
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-08-04 11:18:35 +0100

    - get Kodi details from env vars

--- kodiMonitor.py
+++ kodiMonitor.py
@@ -4,6 +4,12 @@ import os
 from time import sleep
 import RPi.GPIO as GPIO
 
+# Kodi
+KodiUserName = os.environ['KODI_USER_NAME']
+KodiPassword = os.environ['KODI_PASSWORD']
+KodiHost = os.environ['KODI_HOST']
+KodiPort = os.environ['KODI_PORT']
+
 # input buttons
 KodiUp = 14
 KodiBack = 18
@@ -30,6 +36,13 @@ GPIO.setup(KodiLeft, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(KodiEnter, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(KodiRight, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(KodiDown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# diagnostics
+print('Settings:')
+print(f'  KODI_USER_NAME = {KodiUserName}')
+print(f'  KODI_PASSWORD  = {KodiPassword}')
+print(f'  KODI_HOST      = {KodiHost}')
+print(f'  KODI_PORT      = {KodiPort}')
 
 while True:
     if (GPIO.input(KodiUp) == True):
