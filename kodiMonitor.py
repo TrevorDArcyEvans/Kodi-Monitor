859d7ec21489c7fb1c49c7615dde1f6bacec6813
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-08-05 08:26:48 +0100

    - renamed to align with Kodi API

--- kodiMonitor.py
+++ kodiMonitor.py
@@ -27,7 +27,7 @@ KodiLeft = 4
 #
 # $ cat /sys/firmware/devicetree/base/model 
 # Raspberry Pi Model B Rev 1
-KodiEnter = 21
+KodiSelect = 21
 
 KodiRight = 17
 KodiDown = 15
@@ -108,8 +108,8 @@ def OnKodiBack(channel):
 def OnKodiLeft(channel):
   print("OnKodiLeft")
 
-def OnKodiEnter(channel):
-  print("OnKodiEnter")
+def OnKodiSelect(channel):
+  print("OnKodiSelect")
 
 def OnKodiRight(channel):
   print("OnKodiRight")
@@ -125,14 +125,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(KodiUp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(KodiBack, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(KodiLeft, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(KodiEnter, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(KodiSelect, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(KodiRight, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(KodiDown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 GPIO.add_event_detect(KodiUp, GPIO.RISING, callback=OnKodiUp, bouncetime=BounceTime)
 GPIO.add_event_detect(KodiBack, GPIO.RISING, callback=OnKodiBack, bouncetime=BounceTime)
 GPIO.add_event_detect(KodiLeft, GPIO.RISING, callback=OnKodiLeft, bouncetime=BounceTime)
-GPIO.add_event_detect(KodiEnter, GPIO.RISING, callback=OnKodiEnter, bouncetime=BounceTime)
+GPIO.add_event_detect(KodiSelect, GPIO.RISING, callback=OnKodiSelect, bouncetime=BounceTime)
 GPIO.add_event_detect(KodiRight, GPIO.RISING, callback=OnKodiRight, bouncetime=BounceTime)
 GPIO.add_event_detect(KodiDown, GPIO.RISING, callback=OnKodiDown, bouncetime=BounceTime)
 
